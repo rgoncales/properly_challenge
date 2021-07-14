5ee2c087d4cf19a24306d8c5aa9aed85aa1743a5
Rodrigo Goncales <rodrigo.goncales96@gmail.com>
2021-07-13 22:10:06 -0400

    Added ImageCarousel

---
+++ src/components/layout/ImageCarousel/index.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+/*
+ * use some library to display a carousel
+ * added some mock code for now
+ */
+
+const Carousel = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const CarouselItem = styled.div`
+  height: 300px;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+`
+
+const ImageCarousel = (props) => {
+  const renderImages = () => {
+    if (!props.imageList.length) {
+      return <div> No Images </div>
+    }
+    return props.imageList.map((url) => {
+      return (
+        <CarouselItem>
+          <img src={url} alt="house-pic" />
+        </CarouselItem>
+      )
+    })
+  }
+  return <Carousel>{renderImages()}</Carousel>
+}
+
+ImageCarousel.propTypes = {
+  imageList: PropTypes.arrayOf(PropTypes.string),
+}
+
+ImageCarousel.defaultProps = {
+  imageList: [],
+}
+
+export default ImageCarousel
--- src/pages/PropertyView/index.jsx
+++ src/pages/PropertyView/index.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import PageContainer from 'components/layout/PageContainer'
+import styled from 'styled-components'
+import ImageCarousel from 'components/layout/ImageCarousel'
+
+const ImageContainer = styled.div`
+  width: 100%;
+  height: 300px;
+  background: yellow;
+`
+
+const DetailsContainer = styled.div`
+  width: 100%;
+  flex: 1;
+  background: red;
+`
+
+const RelatedPropertiesContainer = styled.div`
+  width: 100%;
+  background: pink;
+`
 
 const NavBar = () => {
-  return <PageContainer>Hello World</PageContainer>
+  return (
+    <PageContainer>
+      <ImageContainer>
+        <ImageCarousel
+          imageList={[
+            'https://media.fromthegrapevine.com/assets/images/2015/3/hobbit-house.jpg.480x0_q71_crop-scale.jpg',
+            'https://media.fromthegrapevine.com/assets/images/2015/3/hobbit-house.jpg.480x0_q71_crop-scale.jpg',
+            'https://media.fromthegrapevine.com/assets/images/2015/3/hobbit-house.jpg.480x0_q71_crop-scale.jpg',
+          ]}
+        />
+      </ImageContainer>
+      <DetailsContainer />
+      <RelatedPropertiesContainer />
+    </PageContainer>
+  )
 }
 
 NavBar.propTypes = {}
